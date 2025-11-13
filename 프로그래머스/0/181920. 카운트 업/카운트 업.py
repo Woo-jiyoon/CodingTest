3794cbcd2614df4943f796cd1abb2618393c91ae
JiYoon Woo <166934670+Woo-jiyoon@users.noreply.github.com>
2025-11-14 03:50:04 +0900

    [level 0] Title: 카운트 업, Time: 0.00 ms, Memory: 9.29 MB -BaekjoonHub

---
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.py
@@ -0,0 +1,6 @@
+def solution(start_num, end_num):
+    
+    # range(시작, 끝+1)를 사용해 범위를 만듦
+    # list()로 감싸서 리스트 형태로 반환
+    # end_num + 1을 함으로써 end_num 까지 포함
+    return list(range(start_num, end_num + 1))
